e45485a446c25c4e63e3503e6606e38edabb3361
Eduardo <jeduardorjx@gmail.com>
2019-07-28 06:44:19 -0600

    Be able to push task into the ToDoList object as an array of objects

--- main.js
+++ main.js
@@ -12,50 +12,69 @@ var toDoListArray = [];
 
 // *** Event Listeners ***
 window.addEventListener('load', handlePageLoad)
-// plusBtn.addEventListener('click', handlePlusBtn)
+plusBtn.addEventListener('click', handlePlusBtn)
 // plusBtn.addEventListener('click', removeTask)
 taskItemInput.addEventListener('keyup', disablePlusBtn)
+taskTitleInput.addEventListener('keyup', disablePlusBtn)
 
 makeTaskListBtn.addEventListener('click', handleMakeTaskListBtn)
 
 // *** Functionality | Handlers 1st ***
 function handlePageLoad() {
   disablePlusBtn();
+  // disableMakeTaskListButton();
+  createNewToDoList()
 }
 
 function handleMakeTaskListBtn() {
-  createNewToDoList();
+  // createNewToDoList();
   // finalizeToDoList();
 }
 
 function handlePlusBtn() {
-  createNewTask();
+  console.log('in HandlePlusBtn')
+  createNewTask(toDoListArray[toDoListArray.length - 1]);
 }
 
 function disablePlusBtn() {
-  taskItemInput.value === '' ? plusBtn.disabled = true : plusBtn.disabled = false;
+  // taskItemInput.value === '' && taskTitleInput.value === '' ? plusBtn.disabled = true : plusBtn.disabled = false;
+  if (taskTitleInput.value === '' || taskItemInput.value === '') {
+    plusBtn.disabled = true;
+  } else {
+    plusBtn.disabled = false;
+  }
+}
+
+function disableTaskListButton() {
+  //how to check if there are any objects in DOM?
+
 }
 
 function createNewToDoList() {
   var newToDoList = new ToDoList({title: taskTitleInput.value});
   toDoListArray.push(newToDoList)
-  createNewTask(newToDoList);
+  console.log(newToDoList)
+  // createNewTask(newToDoList);
 }
 
 function createNewTask(newToDoList) {
+  console.log('in createNewTask')
   var task = new ToDoTask({text: taskItemInput.value});
+  console.log('in createNewTask', task)
+
   addTaskToDom(event, task)
-  console.log('first', newToDoList)
+  // console.log('first', newToDoList)
   newToDoList.addTask(task)
-  console.log('second', newToDoList)
-  // return itemObj
+  // console.log('second', newToDoList)
 }
 
 function addTaskToDom(event, task) {
-  if (event.target.className ==='form__div--plusimg' && taskItemInput.value !== '') {
+  console.log("in addTaskToDom")
+  if (event.target.className === 'form__div--plusbtn' && taskItemInput.value !== '') {
   taskList.insertAdjacentHTML('beforeend', `<li class="ul__li" ><img class="ul__li--deleteimg" src="images/delete.svg" alt="delete img icon">${task.text}</li>`)
   }
-  clearTaskInput();
+  //Activate "Make Task List Button"
+  // clearTaskInput();
 }
 
 function removeTask(event) {
@@ -97,3 +116,34 @@ cardSection.insertAdjacentHTML('afterbegin', `<article>
           </footer>
         </article>`)
 }
+
+
+function createTodoTask() {
+  //You are setting and empty array to 'todotasks'
+  localStorage.setItem('todoTasks', JSON.stringify([]));
+}
+
+
+// //Chris code
+// function cardInstance(e) {
+//   e.preventDefault();
+//   //You setting todoTasks to an empty array by getting 'todoTasks' then parsing it aka todoTasks = []
+//   var todoTasks = JSON.parse(localStorage.getItem('todoTasks'));
+//   //you are instantiating a new instance of the ToDoList class passing it
+//   //the date.now, the Title, and an empty array
+//   var taskCard = new ToDoList(Date.now(), titleInput.value, todoTasks);
+//   //Then you are pushing that new Instance of the ToDoList class into the
+//   //todoList Array
+//   cardsArray.push(taskCard);
+//   //You call saveToStorage on the new instance and pass it in the array to save it to storage
+//   taskCard.saveToStorage(cardsArray);
+//   //If 'todolistArray' is set to something
+//   if (localStorage.getItem('todoListArray')) {
+//     //invoke createtodoTask which sets a stringified empty array
+//     createTodoTask();
+//     //then remove the strinified array from storage
+//     localStorage.removeItem('todoTasks');
+//   }
+//   //invoke generate card passing it the instance of todoList
+//   generateCard(taskCard);
+// }
